b90200aac4818980aa54a00e507fa6217f31ef17
Nico Martinucci <domenico.martinucci@gmail.com>
2023-03-24 14:50:46 -0700

    update app.py with blueprint logic

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 import os
-from dotenv import load_dotenv
 from routes.recipes import recipes
 from routes.users import users
+from models import connect_db
 
 app = Flask(__name__)
-app.register_blueprint(recipes, url_prefix="/api/recipes")
-app.register_blueprint(users, url_prefix="/api/users")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///recipease"
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = "SECREEEEEEEEEEEET"
+
+connect_db(app)
+
+app.register_blueprint(recipes, url_prefix="/api/recipes")
+app.register_blueprint(users, url_prefix="/api/users")
