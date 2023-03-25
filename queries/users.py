5329cd96908f0fe011ab00455f88c94daec2d49c
Nico Martinucci <domenico.martinucci@gmail.com>
2023-03-25 15:15:15 -0700

    add route/function to log in existing user

--- queries/users.py
+++ queries/users.py
@@ -1,20 +1,21 @@
 from models import User, db
 from flask_bcrypt import Bcrypt
-from sqlalchemy.exc import IntegrityError
+import helpers.tokens as token
 
 bcrypt = Bcrypt()
 
 
 def add_new_user(username, email, password, first_name, last_name, bio):
-    """Adds a new user to the database."""
+    """
+    Adds a new user to the database, returning a serialized dictionary if
+    successful or error messages if not.
+    """
 
     errors = []
 
     dupe_username = User.query.get(username)
+    # TODO: does this work as .first()?
     dupe_email = User.query.filter(User.email == email).count()
-
-    print("DUPE_USERNAME", dupe_username)
-    print("DUPE_EMAIL", dupe_email)
 
     if dupe_username:
         errors.append("That username is already taken. Please try again.")
@@ -28,7 +29,7 @@ def add_new_user(username, email, password, first_name, last_name, bio):
     hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
     try:
-        user = User(
+        new_user = User(
             username=username,
             email=email,
             password=hashed_pwd,
@@ -37,15 +38,15 @@ def add_new_user(username, email, password, first_name, last_name, bio):
             bio=bio
         )
 
-        db.session.add(user)
+        db.session.add(new_user)
         db.session.commit()
 
         serialized = {
-            "username": user.username,
-            "email": user.email,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "bio": user.bio,
+            "username": new_user.username,
+            "email": new_user.email,
+            "first_name": new_user.first_name,
+            "last_name": new_user.last_name,
+            "bio": new_user.bio,
         }
 
         return {"user": serialized}
@@ -54,5 +55,19 @@ def add_new_user(username, email, password, first_name, last_name, bio):
         return {"error": "Something went wrong..."}
 
 
-def authenticate_current_user():
-    """Authenticates an existing user."""
+def authenticate_current_user(username, password):
+    """
+    Authenticates an existing user, returning a valid JWT if successful or
+    error messages if not.
+    """
+
+    user = User.query.filter(User.username == username).first()
+
+    if user:
+        is_auth = bcrypt.check_password_hash(user.password, password)
+        if not is_auth:
+            return {"error": "Invalid username/password combination. Please try again."}
+
+        return {"token": token.get_jwt(username)}
+
+    return {"error": "Username not found. Please try again."}
--- routes/users.py
+++ routes/users.py
@@ -6,7 +6,7 @@ users = Blueprint("users", __name__)
 
 @users.route("/signup", methods=["POST"])
 def signup_user():
-    """Registers a new user"""
+    """Registers a new user."""
 
     new_user = q.add_new_user(
         username=request.json["username"],
@@ -18,3 +18,15 @@ def signup_user():
     )
 
     return jsonify(new_user)
+
+
+@users.route("/login", methods=["POST"])
+def login_user():
+    """Authenticates an existing user."""
+
+    auth_user = q.authenticate_current_user(
+        username=request.json["username"],
+        password=request.json["password"]
+    )
+
+    return jsonify(auth_user)
